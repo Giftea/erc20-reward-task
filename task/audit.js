7b574ecb013281044cdf2a7b061172cb8cc37f7c
giftea <eleojogiftuhiene@gmail.com>
2023-08-23 01:41:18 +0100

    Updated the audit feature

--- task/audit.js
+++ task/audit.js
@@ -1,23 +1,32 @@
+require('dotenv').config();
+const { Web3 } = require('web3');
+const web3 = new Web3(`https://goerli.infura.io/v3/${process.env.INFURA_ID}`);
 const { namespaceWrapper } = require('../_koiiNode/koiiNode');
 
 class Audit {
   async validateNode(submission_value, round) {
-    // Write your logic for the validation of submission value here and return a boolean value in response
-
-    // The sample logic can be something like mentioned below to validate the submission
     let vote;
     console.log('SUBMISSION VALUE', submission_value, round);
     try {
-      if (submission_value == 'Hello, World!') {
-        // For successful flow we return true (Means the audited node submission is correct)
-        vote = true;
+      const txReceipt = await web3.eth.getTransactionReceipt(submission_value);
+
+      if (txReceipt) {
+        if (txReceipt.status) {
+          // For successful flow we return true (Means the audited node submission is correct)
+          vote = true;
+          console.log('Transaction is successful');
+        } else {
+          // For unsuccessful flow we return false (Means the audited node submission is incorrect)
+          vote = false;
+          console.log('Transaction has failed');
+        }
       } else {
-        // For unsuccessful flow we return false (Means the audited node submission is incorrect)
         vote = false;
+        console.log('Transaction not found');
       }
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
       vote = false;
+      console.error('Error fetching transaction status:', error);
     }
     return vote;
   }
